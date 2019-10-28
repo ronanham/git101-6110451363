70513615cf21e4ebe8aec5c01afc2f3692f1df3d
ronakorn <ronakorn.s@ku.th>
2019-10-28 14:15:18 +0700

    Add lab1001.cpp

---
+++ lab1001.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Hero{
+private:
+    string name;
+    int level;
+public:
+     Hero(string name, int level){
+         this->name = name;
+         this->level = level;
+    }
+    string getName(){
+        return name;
+    }
+    int getLevel(){
+        return level;
+    }
+};
+int main()
+{
+   
+}
